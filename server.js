ebda0cb4b1cc0000c000dedb36864a512ea2c3e1
Giulio Garzia <32651700+Ozozuz@users.noreply.github.com>
2020-05-05 12:31:42 +0200

    cleanup

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const port = process.env.PORT;
-const app  = require('./app');
-const server = http.createServer(app);
-
-server.listen(port, ()=>{
-    console.log('Server running on port 3000');
-});
